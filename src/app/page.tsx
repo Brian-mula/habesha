3959bbcd66ca77e0f3f9a64a081f3888ad1873b5
Brian-mula <mulatiwangilabrian@gmail.com>
2024-09-11 12:18:12 +0300

    Fix:overflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,8 +50,8 @@ const flashUrl = "https://flashsport.bet/"
     console.log(script);
   }, []);
   return (
-    <div className="vamos-bg">
-      <div className="w-screen flex justify-start lg:justify-center flex-col items-center py-16 px-4 lg:px-36 h-screen mx-auto bg-gray-800/80 ">
+    <div className="vamos-bg overflow-y-auto">
+      <div className="w-screen flex justify-start lg:justify-center flex-col items-center py-16 px-4 lg:px-36 mx-auto bg-gray-800/80 ">
         <Image
           src="/image/habesha.png"
           alt="Vamos Logo"
